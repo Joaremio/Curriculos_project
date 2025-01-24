621273b7f3d3431970d02d888c4057fb82360f5f
Joaremio <joaremiorevoredo@gmail.com>
2025-01-23 21:17:51 -0300

    fix: fix anottation

--- src/main/java/br/ufrn/imd/CurriculoRepository.java
+++ src/main/java/br/ufrn/imd/CurriculoRepository.java
@@ -1,6 +1,8 @@
 package br.ufrn.imd;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CurriculoRepository extends JpaRepository<Curriculo, Long> {
 }
